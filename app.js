d64b77b4dc0dea11909e4b7289b62e219833df83
Kohei Iwasaki <iwasaki.kohei@donuts.ne.jp>
2014-04-05 14:18:52 +0900

    websocket

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
-
 /**
  * Module dependencies.
  */
-
+ 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
+var WSServer = require('websocket').server;
+var url = require('url');
 var path = require('path');
-
+ 
 var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
+var server = require('http').createServer(app);
+ 
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+ 
+app.configure('development', function(){
   app.use(express.errorHandler());
-}
+});
+ 
+server.listen(app.get('port'))
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+var webSocketServer = new WSServer({ httpServer: server });
+var accept = ['localhost', '127.0.0.1'];
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+webSocketServer.on('request', function(req){
+	req.origin = req.origin || '*';
+	if(accept.indexOf(url.parse(req.origin).hostname) === -1){
+		req.reject();
+		console.log(req.origin + 'からのアクセスは許可されていません。');
+		return;
+	}
+
+	var websocket = req.accept(null, req.origin);
+
+	websocket.on('message',function(msg){
+		console.log('"' + msg.utf8Data + '"を' + req.origin + 'から受信');
+		if(msg.utf8Data === 'Hello'){
+			websocket.send('WebSocketサーバからこんにちは！');
+		}
+	});
+
+	websocket.on('close', function(code, desc){
+		console.log('接続解除：' + code + '-' + desc);
+	});
 });
