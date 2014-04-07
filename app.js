e528ac4cb3b34525676f02515455a45d3120d875
Kohei Iwasaki <iwasaki.kohei@donuts.ne.jp>
2014-04-07 20:24:51 +0900

    load quizes

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ var mongoose = require('mongoose');
 
 var schema = require('./schema.js');
 
+var fs = require('fs');
+var lineList = fs.readFileSync('QuizList.csv').toString().split('\r\n');
+lineList.shift();
+var quizKeyList = ['index','category','contents','correct_answer','wrong_answer'];
+var quizes=[[],[],[],[],[],[]];
+while(lineList.length){
+  var line = lineList.shift();
+  console.log(line);
+  var doc = {};
+  line.split(',').forEach(function (entry, i) {
+    doc[quizKeyList[i]] = entry;
+  });
+  quizes[doc.category-1].push(doc);
+}
+console.log(quizes[0]);
+
 // 定義フェーズ
 mongoose.model('User', schema.user);
 mongoose.model('Trolley', schema.trolley);
