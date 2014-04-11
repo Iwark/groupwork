a0926298cfb556be7e317bfc9f68178054f26f5f
Kohei Iwasaki <iwasaki.kohei@donuts.ne.jp>
2014-04-11 16:00:52 +0900

    そろそろや

--- app.js
+++ app.js
@@ -226,8 +226,8 @@ wss.on('connection', function(ws){
               Trolley.findOne({ _id: user.trolley_id }, function(err, trolley){
                 if(!err && trolley){
                   for(var i = 0; i<trolley.users.length; i++){
-                    console.log(trolley.users[i]._id+":"+user._id);
-                    if(trolley.users[i].hasOwnProperty('_id') && trolley.users[i]._id == user._id){
+                    console.log(String(trolley.users[i]._id) == String(user._id));
+                    if(String(trolley.users[i]._id) == String(user._id)){
                       trolley.users[i] = user;
                       console.log("saving user");
                       break;
