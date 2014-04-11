04a27f8bc93d25d633613df8aa6cd28ba7261104
Kohei Iwasaki <iwasaki.kohei@donuts.ne.jp>
2014-04-11 09:54:47 +0900

    fix bug

--- actions.js
+++ actions.js
@@ -33,7 +33,7 @@ module.exports={
     User.findOne({ _id: user_id}, function(err, user){
       if(!err && user.trolley_id){
         Trolley.findOne({ _id: user.trolley_id}, function(err, trolley){
-          if(!err){
+          if(!err && trolley){
             trolley.users = trolley.users.filter(function(u, i){
               if(u._id === user_id) return false;
               else return true;
